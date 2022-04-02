6c526713a8a854da6af07f5c4200ac11954ecfed
DevEmmy <eolaosebikan60@gmail.com>
2022-04-02 18:20:00 +0100

    Updated Strapi and Real estate component

--- src/Components/Others/RealEstate.jsx
+++ src/Components/Others/RealEstate.jsx
@@ -24,7 +24,7 @@ function RealEstate() {
 
 
     const fetchRealEstate = async ()=>{
-      const resp = await fetch('http://localhost:1337/api/real-estate-houses')
+      const resp = await fetch('http://localhost:1337/api/real-estate-houses?populate=*')
       const data = await resp.json()
       console.log(data.data)
       setListing(data.data)
@@ -60,7 +60,14 @@ function RealEstate() {
                 listing.map((list)=>{
                   return(
                     <div className='list'>
-                      {list.attributes.title}
+                      <img src={list.attributes.media.data[0].attributes.url} alt="" />
+                      <div className="list_text">
+                        <h3>{list.attributes.title}</h3>
+                        <h6>Category : { list.attributes.real_estate_subcategory.data.attributes.category}</h6>
+                        <p>
+                          {list.attributes.description}
+                        </p>
+                      </div>
                     </div>
                   )
                 })
