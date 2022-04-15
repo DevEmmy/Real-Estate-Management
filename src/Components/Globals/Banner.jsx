f4a49247f2a1ed0f4d73f14e40c48f251cf561f8
DevEmmy <eolaosebikan60@gmail.com>
2022-04-15 19:42:15 +0100

    Added new feature

--- src/Components/Globals/Banner.jsx
+++ src/Components/Globals/Banner.jsx
@@ -1,3 +1,4 @@
+import { ChevronDoubleDownIcon } from '@heroicons/react/solid'
 import React from 'react'
 import {info} from '../Assets/Details/info'
 
@@ -6,6 +7,10 @@ function Banner() {
     <div className='banner' style={{backgroundImage: `linear-gradient(rgb(0,0,0,0.3), rgba(0,0,0,0.7)), url(${info.banner_img})`}}>
                 <h1>{info.banner_text}</h1>
                 <p>{info.banner_sub_text}</p>
+
+                <div className="scroll_down_icon">
+                  <ChevronDoubleDownIcon width={50}/>
+                </div>
     </div>
   )
 }
