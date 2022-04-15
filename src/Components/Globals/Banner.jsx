97d661e3367a7c896bb33ce6a81f4958b7020e16
DevEmmy <eolaosebikan60@gmail.com>
2022-04-15 18:49:25 +0100

    Little Change

--- src/Components/Globals/Banner.jsx
+++ src/Components/Globals/Banner.jsx
@@ -3,7 +3,7 @@ import {info} from '../Assets/Details/info'
 
 function Banner() {
   return (
-    <div className='banner' style={{backgroundImage: `url(${info.banner_img})`}}>
+    <div className='banner' style={{backgroundImage: `linear-gradient(rgb(0,0,0,0.3), rgba(0,0,0,0.7)), url(${info.banner_img})`}}>
                 <h1>{info.banner_text}</h1>
                 <p>{info.banner_sub_text}</p>
     </div>
