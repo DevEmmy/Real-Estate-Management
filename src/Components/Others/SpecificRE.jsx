f121a97dec96f6942c0bad8671fc9d11d52906c1
DevEmmy <eolaosebikan60@gmail.com>
2022-04-28 09:08:41 -0700

    relative real estate

--- src/Components/Others/SpecificRE.jsx
+++ src/Components/Others/SpecificRE.jsx
@@ -1,3 +1,4 @@
+import { CameraIcon, HeartIcon } from '@heroicons/react/solid'
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { apiURLs } from '../Assets/Details/api'
@@ -6,12 +7,12 @@ import Nav from '../Globals/Nav'
 function SpecificRE() {
 
   const { id } = useParams()
-  const [listing, setListing] = useState()
+  const [listing, setListing] = useState([])
 
   const fetchRealEstate = async ()=>{
     const resp = await fetch(apiURLs.fetchRealEstate)
     const data = await resp.json()
-    console.log(data.data.filter(i=> i.id == id))
+    console.log(data.data)
     setListing(data.data.filter(i=> i.id == id))
   }
 
@@ -38,7 +39,23 @@ function SpecificRE() {
     <div className='sp_real_estate' >
       <Nav width = {width}/>
 
-        <div className="spl_top_section" style={{backgroundImage: `linear-gradient(rgb(0,0,0,0.3), rgba(0,0,0,0.7)), url(${ listing[0]?.attributes.media.data[0].attributes.url})`}}>
+        <div className="spl_top_section" style={{backgroundImage: `linear-gradient(rgb(0,0,0,0.3), rgba(0,0,0,0.7)), url(${ listing[0]?.attributes.media.data[0].attributes.url})`, height: '600px'}} >
+
+          <div className="list_title">
+            {
+              listing[0]?.attributes.title
+            }
+          </div>
+            <div className="list_option">
+              <div className="opt">
+                save <HeartIcon width={22}/>
+              </div>
+
+              <div className="opt">
+                view photos <CameraIcon width={22} />
+              </div>
+            </div>
+        </div>
 
  
             {
@@ -47,15 +64,15 @@ function SpecificRE() {
                   <div key={i.id} className="sp_list">
                     {/* <img src={i.attributes.media.data[0].attributes.url} alt="" /> */}
 
-                    <div className="sp_list_content">
+                    {/* <div className="sp_list_content">
                       <h3>{i.attributes.title}</h3>
-                    </div>
+                    </div> */}
                     
                   </div>
                 )
               })
             }
-            </div>
+           
     </div>
   )
 }
